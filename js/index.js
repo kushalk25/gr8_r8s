0844b71d0b2f313f91e6d8516e035d848b5c36f7
mishalbashir <mishaal.albashir@gmail.com>
2017-03-25 18:58:14 -0400

    Get Movie names from the array

--- js/index.js
+++ js/index.js
@@ -2,12 +2,28 @@
 $('#search_bar').on('submit', function (e) {
     title = e.target[0].value
 
-    fetch_url = 'http://www.omdbapi.com/?t=' + title
-    result = $.ajax ({
+//
+    var fetch_url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+   fetch_url += '?' + $.param({
+     'api-key': "8f50df51604143d9af719239e37befd1",
+     'query': title
+   });
+
+    $.ajax ({
       type: 'GET',
       url: fetch_url,
       success: function(movies) {
-          document.getElementById('content').innerHTML = movies.Metascore
+//          console.log(movies.results[0].display_title);
+          debugger
+          var movieTitles = movies.results.display_title;
+
+          $.each(movies.results, function(index, movie){
+            debugger
+            var movieTitles = movie.display_title;
+            console.log(movieTitles);
+
+          });
+
       }
 
     });

4b92a943a0ab3f047da2c743e67b8448120c9fe3
Kushal Kumarasinghe <kushal.k@hotmail.com>
2017-03-25 19:29:18 -0400

    Displayed names properly

--- js/index.js
+++ js/index.js
@@ -2,30 +2,27 @@
 $('#search_bar').on('submit', function (e) {
     title = e.target[0].value
 
-//
     var fetch_url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-   fetch_url += '?' + $.param({
-     'api-key': "8f50df51604143d9af719239e37befd1",
-     'query': title
-   });
+    fetch_url += '?' + $.param({
+      'api-key': "8f50df51604143d9af719239e37befd1",
+      'query': title
+    });
 
     $.ajax ({
       type: 'GET',
       url: fetch_url,
       success: function(movies) {
-//          console.log(movies.results[0].display_title);
-          debugger
+          content_div = document.getElementById('content')
           var movieTitles = movies.results.display_title;
 
           $.each(movies.results, function(index, movie){
-            debugger
             var movieTitles = movie.display_title;
-            console.log(movieTitles);
-
+            var newDiv = document.createElement("div");
+            newDiv.appendChild(document.createTextNode(movie.display_title));
+            content_div.appendChild(document.createElement("br"));
+            content_div.append(newDiv)
           });
-
       }
-
     });
 
     return false;
